a8c09ad83451bc173657592a577ed97f476d0803
andyarensman <86622025+andyarensman@users.noreply.github.com>
2024-05-23 17:58:08 -0700

    basic staff story

--- src/stories/Staff.stories.tsx
+++ src/stories/Staff.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Staff } from "./Staff";
+import { Measure } from "./Measure";
+import { Note } from "./Note";
 
 const meta: Meta<typeof Staff> = {
   component: Staff,
@@ -10,5 +12,32 @@ export default meta;
 type Story = StoryObj<typeof Staff>;
 
 export const Primary: Story = {
-  render: () => <Staff />,
+  render: (args) => (
+    <Staff>
+      <Measure clef="gClef">
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+      </Measure>
+      <Measure>
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+      </Measure>
+      <Measure>
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+      </Measure>
+      <Measure>
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+      </Measure>
+      <Measure>
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+      </Measure>
+      <Measure>
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+        <Note pitch={{ position: "line-4" }} noteValue="half" />
+      </Measure>
+    </Staff>
+  ),
 };
